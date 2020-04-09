a1c45b09a24620b172c303846afb3d015bdddfbd
wilofitz <gfitzpatrick@sistemasw.com.ar>
2020-04-09 12:06:46 -0300

    fixes service description and adds pelba license

--- install-svc.bat
+++ install-svc.bat
@@ -1,15 +1,11 @@
 @echo off
 rem run this script as admin
 
-if not exist aa-connector.exe (
-    echo Build the example before installing by running "go build -o ../aa-conector.exe" in src folder
-    goto :exit
-)
+
 
 sc create aa-connector-svc binpath= "%CD%\aa-connector.exe" start= auto DisplayName= "aa-connector-svc"
-sc description aa-connector "afip-arba connector windows service"
+sc description aa-connector-svc "afip-arba connector windows service"
 sc start aa-connector-svc
-sc query aa-connector-svc
 
 echo Check aa-connector.log
 
